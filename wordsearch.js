1fb9cf41ab28aea564eebb4658f5574ed9ca4c9e
Tobi Onipede <onipede89@gmail.com>
2023-06-19 02:30:41 +0100

    solve bug in code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = matrix => matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+    let found = false;
+
+    for (let i = 0; i < horizontalJoin.length; i++) {
+        if (horizontalJoin[i] && horizontalJoin[i].includes(word)) {
+            found = true;
+            break;
+        }
+        if (verticalJoin[i] && verticalJoin[i].includes(word)) {
+            found = true;
+            break;
+        }
+    }
+
+    return found;
+};
+
+module.exports = wordSearch;
